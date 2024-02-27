b96b03283a65aaaf21c8bbfdc98da9c27687ddaa
imtyaz-17 <imtyazit17017@gmail.com>
2024-02-27 20:19:10 +0600

    ms-6 module-35 completed

---
+++ JavaScript/explore-js-objects/object.js
@@ -0,0 +1,13 @@
+// create object using literals
+const player = {};
+player.name = 'imtyaz';
+
+const student = {
+    name: 'imtyaz',
+    id: 1,
+    play: function () {
+        console.log('cricket');
+    },
+    marks: 95
+}
+console.log(student);
