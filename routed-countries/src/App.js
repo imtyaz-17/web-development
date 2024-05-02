4379b1f0eba682315de0e7c760cfa82ddc96ac5b
imtyaz-17 <imtyazit17017@gmail.com>
2024-05-03 00:05:59 +0600

    ms-9 mdl 52 complete

---
+++ routed-countries/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/routes';
+
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
