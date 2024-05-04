3c37bbc4c07bbaca0314b4508254ba56fec5634c
imtyaz-17 <imtyazit17017@gmail.com>
2024-05-04 16:26:55 +0600

    ms-9 mdl 53 complete

---
+++ ema-john-simple-router/src/components/Cart/Cart.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Cart.css';
+const Cart = (props) => {
+    const { cart, clearCart } = props;
+    let total = 0;
+    let shippingCost = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        quantity = quantity + product.quantity;
+        total = total + (product.price * product.quantity);
+        shippingCost = shippingCost + product.shipping;
+    }
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shippingCost + tax;
+    return (
+        <div className='cart'>
+            <div>
+                <h3>Order Summary</h3>
+                <p>Selected Items: {quantity}</p>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping: ${shippingCost}</p>
+                <p>Tax: ${tax}</p>
+                <p>Grand Total: ${grandTotal}</p>
+            </div>
+
+            <div>
+                <button onClick={clearCart}>Clear Cart</button>
+            </div>
+        </div>
+    );
+};
+
+export default Cart;
---
+++ ema-john-simple-router/src/components/Orders/Orders.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
+import ItemInCart from '../ItemInCart/ItemInCart';
+import { deleteShoppingCart, removeFromDb } from '../../utilities/localdb';
+
+const Orders = () => {
+    const { products, initialCart } = useLoaderData();
+    const [cart, setCart] = useState(initialCart);
+    const clearCart = () => {
+        setCart([]);
+        deleteShoppingCart();
+    }
+    const handleRemoveFromCart = (id) => {
+        const remainingItems = cart.filter(product => product.id !== id);
+        setCart(remainingItems);
+        removeFromDb(id);
+    }
+    return (
+        <div className='shop-container'>
+            <div className='orders-container'>
+                {
+                    cart.map(product => <ItemInCart key={product.id} product={product} handleRemoveFromCart={handleRemoveFromCart}></ItemInCart>)
+                }
+                {
+                    cart.length === 0 && <h2>No Items in Cart. Please <Link to='/'>Shop more</Link></h2>
+                }
+            </div >
+            <div className='cart-container'>
+                <Cart cart={cart} clearCart={clearCart}></Cart>
+            </div>
+        </div >
+    );
+};
+
+export default Orders;
