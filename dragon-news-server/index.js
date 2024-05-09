20abda9c8cce9f8c13f5cb2090abc9d037f3b706
imtyaz-17 <imtyazit17017@gmail.com>
2024-05-09 21:13:33 +0600

    module - 61 server

--- dragon-news-server/index.js
+++ dragon-news-server/index.js
@@ -5,13 +5,32 @@ const port = process.env.PORT || 5000
 
 app.use(cors());
 const categories = require('./data/category.json');
-
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('News API runnig')
 })
 app.get('/news-categories', (req, res) => {
     res.send(categories)
+})
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    } else {
+        const categoryNews = news.filter(ns => ns.category_id === id);
+        res.send(categoryNews);
+    }
+
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(ns => ns._id === id);
+    res.send(selectedNews);
 })
 app.listen(port, () => {
     console.log(`news app listening on port ${port}`)
